00b2381628840b300f39db305dad74a48c47c049
Mark Evans <SerMock@users.noreply.github.com>
2018-11-27 18:41:45 -0700

    Initial commit
    Upload of project

---
+++ frontend-nanodegree-arcade-game/README.md
@@ -0,0 +1,5 @@
+# Classic Arcade Game Clone Project
+
+To play, press the arrow keys to move your character. Avoid the insects and reach the water to win! The game is endless and will keep resetting your character.
+
+Great for mindless fun!
---
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -0,0 +1,113 @@
+// --- Enemies --- ///
+
+// Enemie movement
+let Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image/sprite for our enemies
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position.
+Enemy.prototype.update = function(dt) {
+    this.x += this.speed * dt;
+
+    // When off canvas, reset the position of the enemy to move across again
+    if (this.x > 550) {
+        
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }
+
+    // Check for collision between player and enemies
+    if (player.x < this.x + 60 &&
+        player.x + 37 > this.x &&
+        player.y < this.y + 25 &&
+        30 + player.y > this.y) {
+        player.x = 200;
+        player.y = 380;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// --- Player --- //
+let Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function() {
+    // Prevent the player from moving beyond canvas boundary
+    if (this.y > 380) {
+        this.y = 380;
+    }
+
+    if (this.x > 400) {
+        this.x = 400;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // Check for the player reaching top of canvas
+    if (this.y < 0) {
+        this.x = 200;
+        this.y = 380;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    switch (keyPress) {
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+    }
+};
+
+// --- Objects --- //
+let allEnemies = [];
+
+// Position "y" is where the enemies will be created
+let enemySpawner = [60, 140, 220];
+let player = new Player(200, 380, 50);
+let enemy;
+
+enemySpawner.forEach(function(posY) {
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    let allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
